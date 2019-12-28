4aeaf62ea3a819a6443ba65a2612bbd2e087231c
wagnermarques <wagnerdocri@gmail.com>
2019-12-27 23:07:48 -0300

    ajuste no script wget

--- fedora-multimedia-box/scripts/wget-rAFiles.sh
+++ fedora-multimedia-box/scripts/wget-rAFiles.sh
@@ -1,4 +1,7 @@
 #/bin/bash
-fileExtension=$1
+fileExtensions=$1
 url=$2
-wget -r -A.$fileExtension  $url
+
+wget --recursive --no-parent --no-clobber --show-progress --accept $fileExtensions  $url
+
+#wget -r --no-parent --accept mp3,MP3 -nd --span-hosts $url
